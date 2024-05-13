8a818047e83482d0ad07b492710012290a034062
andreabotti <andrea.botti@gmail.com>
2024-05-13 21:24:53 +0200

    v1.02
    Updated table and improve functionality

--- epd_library.py
+++ epd_library.py
@@ -1,9 +1,9 @@
 # IMPORT LIBRARIES
 # from fn__libraries import *
 
-import streamlit as st
-import pandas as pd
-import os
+import os, streamlit as st, pandas as pd
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
+
 
 
 
@@ -27,10 +27,17 @@ st.divider()
 
 
 
-
+#####
 def load_data():
     # Define the path to the CSV file
-    data_path = os.path.join('epdx_data', 'epditaly_gwp.csv')
+    # data_path = os.path.join(
+    #     'epdx_data',
+    #     'epditaly_gwp.csv',
+    #     )
+    data_path = os.path.join(
+        'epdx_data',
+        'master_epd_data.csv',
+        )
     
     # Check if the file exists
     if os.path.exists(data_path):
@@ -41,18 +48,96 @@ def load_data():
         return pd.DataFrame()  # Return an empty DataFrame if file is not found
 
 
-# Set the title of the app
-st.markdown('##### Parsed data from EPD Italy')
+
+#####
+
+col_01, col_02 = st.columns([3,2])
 
 # Load data
-data = load_data()
+df = load_data()
+df_plot = load_data()
 
-# Check if the DataFrame is not empty
-if not data.empty:
-    st.dataframe(
-        data,
-        height=600,
-        )
+# Function to make links clickable
+def make_clickable(url, text):
+    return f'<a href="{url}" target="_blank">{text}</a>'
+
+
+# Apply the make_clickable function to URL and PDF Link columns
+df_plot['entry_url'] = df_plot.apply(
+    lambda x: make_clickable(x['entry_url'], 'Entry link'), axis=1,
+    )
+df_plot['pdf_url'] = df_plot.apply(
+    lambda x: make_clickable(x['pdf_url'], 'EPD PDF Link'), axis=1,
+    )
+
+df.drop(
+    ['entry_url', 'pdf_url', 'image', 'CPC_code', 'cat',
+     'product_description', 'production_unit', 'additional_info',
+     'epd_update_date', 'ref_year',
+    ],
+    axis=1, inplace=True)
+
+
+
+with col_01:
+
+    # Configure AgGrid options
+    gb = GridOptionsBuilder.from_dataframe(df)
+    gb.configure_pagination(paginationAutoPageSize=True)  # Add pagination
+    gb.configure_side_bar()  # Add a sidebar
+    gb.configure_selection(
+        selection_mode="single",
+        use_checkbox=False,
+        )  # Enable single row selection
+
+    grid_options = gb.build()
+
+    # Display the DataFrame using AgGrid
+    grid_response = AgGrid(
+        df,
+        gridOptions=grid_options,
+        update_mode=GridUpdateMode.SELECTION_CHANGED,
+        allow_unsafe_jscode=True,  # Allowing HTML in AgGrid
+        # theme='STREAMLIT',
+    )
+
+    # Get selected rows
+    selected_rows = grid_response['selected_rows']
+
+# Check if any row is selected
+if selected_rows:
+    selected_reg_number = selected_rows[0]['reg_number']
+    
+    # Filter the second DataFrame based on the selected Registration Number
+    sel_df_plot = df_plot[df_plot['reg_number'] == selected_reg_number]
+
+
+    sel_df_plot.drop(
+        ['image',
+        #  'product_description', 'production_unit',
+         'additional_info', 'CPC_code',
+        'epd_update_date', 'ref_year', 'cat',
+        ],
+        axis=1, inplace=True)
+
+    sel_df_plot = sel_df_plot[[
+        'manufacturer',
+        'product_name',
+        'production_unit',
+        'product_type',
+        'entry_url',
+        'pdf_url',
+        'product_description',
+        'reg_number', 'epd_issue_date', 'epd_expiry_date',
+    ]]
+
+    col_02.markdown('##### Data for...')
+    col_02.markdown(
+        sel_df_plot.transpose().to_html(escape=False, index=True),
+        unsafe_allow_html=True,
+    )
+
+
 else:
-    st.error("Error: No data found. Please check the file path and name.")
+    col_02.write("Select a row to see details.")
 
